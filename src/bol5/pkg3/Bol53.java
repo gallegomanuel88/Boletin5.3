6210fd18c134a6e48dccc2b222f959f76b91d3e3
manu <manu@manu-HP-Pavilion-g6-Notebook-PC>
2015-10-20 09:24:25 +0200

    Aplicacion corregida por nina

--- src/bol5/pkg3/Bol53.java
+++ src/bol5/pkg3/Bol53.java
@@ -10,7 +10,7 @@ public class Bol53 {
     public static void main(String[] args) {
         Circulo obj1 = new Circulo ();
         String respuesta1 = JOptionPane.showInputDialog("Radio: ");
-        obj1.radio(Double.parseDouble(respuesta1));
+        obj1.setRadio(Double.parseDouble(respuesta1));
         
         JOptionPane.showMessageDialog(null, "Area= "+obj1.getArea());
         JOptionPane.showMessageDialog(null, "Longitud= "+obj1.getLongitud());
